d776a9d2c1ab31992ee5306a6f457967edf48e3c
Ace <acekiron@outlook.com>
2023-05-29 18:26:43 +0200

    Updated main avatar

--- Main/script.lua
+++ Main/script.lua
@@ -1,4 +1,4 @@
--- vanilla_model.PLAYER:setVisible(false)
+vanilla_model.PLAYER:setVisible(false)
 
 events.ENTITY_INIT:register(function ()
     nameplate.ENTITY:setText("Ace (" .. player:getName() .. ")")
